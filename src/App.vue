9fd4066fa2cee6dd050f6ec4ff894a60d624ddaa
Ricardo Boss <contact@ricardoboss.de>
2022-03-22 23:55:31 +0100

    Added graph

--- src/App.vue
+++ src/App.vue
@@ -35,22 +35,50 @@
 				</p>
 			</template>
 		</template>
+		<hr>
+		<span>
+			<label>
+				Show live graph:
+				<input type="checkbox" v-model="showLiveChart">
+			</label>
+		</span>
+		<line-chart v-if="showLiveChart || gameOver" id="chart" :chartData="foundChartData" :options="chartOptions"></line-chart>
 	</div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
+import {
+	CategoryScale,
+	Chart,
+	Legend,
+	LinearScale,
+	LineController,
+	LineElement,
+	PointElement,
+	Title,
+	Tooltip
+} from "chart.js";
+import {LineChart} from "vue-chart-3";
+
+Chart.register(LineController, CategoryScale, LinearScale, PointElement, LineElement, Legend, Title, Tooltip);
 
 const states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
 const timeLimit = 600;
 
+interface FoundAtEntry {
+	timestamp: number;
+	state: string;
+}
+
 export default defineComponent({
 	name: 'App',
-
+	components: {LineChart},
 	data: () => ({
 		states,
 		guess: '',
 		found: [] as string[],
+		foundAt: [] as FoundAtEntry[],
 		available: [] as string[],
 		message: '',
 		gameStarted: false,
@@ -58,6 +86,39 @@ export default defineComponent({
 		gameWon: false,
 		timer: -1,
 		timerHandle: -1,
+		showLiveChart: false,
+		chartOptions: {
+			responsive: true,
+			interaction: {
+				mode: 'index',
+				intersect: false,
+			},
+			stacked: false,
+			plugins: {
+				legend: {
+					position: 'top',
+				},
+				title: {
+					display: true,
+					text: 'States guessed per second',
+				},
+			},
+			scales: {
+				y: {
+					type: 'linear',
+					display: true,
+					position: 'left',
+				},
+				y1: {
+					type: 'linear',
+					display: true,
+					position: 'right',
+					grid: {
+						drawOnChartArea: false, // only want the grid lines for one axis to show up
+					},
+				},
+			}
+		}
 	}),
 
 	methods: {
@@ -96,11 +157,63 @@ export default defineComponent({
 				let matched = this.available.splice(idx, 1);
 				this.found.push(matched[0]);
 				this.found.sort();
+				this.foundAt.push({timestamp: this.timer, state: matched[0]});
 				this.guess = '';
 
 				if (this.available.length === 0) {
 					this.endGame();
 				}
+			}
+		}
+	},
+
+	computed: {
+		foundChartData() {
+			let points = [];
+			let statesPerSecond = 0;
+			let averageStatesPerSecond = 0;
+			let lastTimestamp = 0;
+			let stateCount = 1;
+			let averageWeight = 0.9;
+			for (let timestamp of this.foundAt.map(entry => timeLimit - entry.timestamp)) {
+				if (lastTimestamp > 0) {
+					statesPerSecond = (stateCount - 1) / timestamp;
+					averageStatesPerSecond = averageWeight * averageStatesPerSecond + (1 - averageWeight) * statesPerSecond;
+				}
+
+				points.push({timestamp, statesPerSecond, averageStatesPerSecond, stateCount});
+				stateCount++;
+				lastTimestamp = timestamp;
+			}
+
+			// throw away first measurement
+			points.shift();
+
+			return {
+				labels: [...Array(timeLimit - this.timer).keys()].map(i => i + 1).slice(1),
+				datasets: [
+					{
+						label: 'Average States Per Second',
+						data: points.map(point => {
+							return {x: point.timestamp, y: point.averageStatesPerSecond};
+						}),
+						borderColor: '#3c8',
+						tension: 0.2,
+					},
+					{
+						label: 'Raw States Per Second',
+						data: points.map(point => { return {x: point.timestamp, y: point.statesPerSecond}; }),
+						borderColor: '#aaaaaa99',
+						tension: 0.1,
+					},
+					{
+						label: 'Total States Found',
+						data: points.map(point => { return {x: point.timestamp, y: point.stateCount}; }),
+						borderColor: '#dd9999aa',
+						tension: 0.1,
+						yAxisID: 'y1',
+					},
+				]
 			}
 		}
 	}
@@ -122,7 +235,7 @@ html, body {
 	align-items: center;
 	justify-content: center;
 	width: 40vw;
-	min-width: 350px;
+	min-width: 400px;
 }
 
 #game-over {
@@ -133,6 +246,10 @@ html, body {
 #game-won {
 	color: green;
 	font-size: 2em;
+}
+
+#chart {
+	width: 100%;
 }
 
 h1 {
@@ -152,7 +269,7 @@ hr {
 	border-top: 1px solid #ccc;
 }
 
-button, input {
+button, input[type='text'] {
 	font-size: 1.25em;
 	padding: 0.5em;
 	margin: 0.5em;
